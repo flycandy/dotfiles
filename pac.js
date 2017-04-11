6426e007c3403747f5de7efd9bd76ff3f933e2de
Youze Tang <tangyouze@gmail.com>
2017-04-11 15:42:07 +0800

    add github and stackoverflow

--- pac.js
+++ pac.js
@@ -14,55 +14,58 @@ var PROXY_DROPLET_SG = 'SOCKS5 ubuntu.urwork.qbtrade.org:1081';
 
 // Feel free to add or edit custom list
 var RULES = [
-      ".facebook.com",
-      ".dropbox.com",
-      ".fbcdn.net",
-      ".tensorflow.org",
-      ".googlevideo.com",
-      ".ytimg.com",
-      ".ggpht.com",
-      ".youtube.com",
-      ".googleapis.com",
-      ".gstatic.com",
-      ".googleusercontent.com",
-      ".twitter.com",
-      ".w3schools.com",
-      ".dropbox.com",
-      ".poloniex.com"
+  ".facebook.com",
+  ".dropbox.com",
+  ".github.com",
+  ".stackoverflow.com",
+  ".fbcdn.net",
+  ".tensorflow.org",
+  ".googlevideo.com",
+  ".ytimg.com",
+  ".ggpht.com",
+  ".youtube.com",
+  ".googleapis.com",
+  ".gstatic.com",
+  ".googleusercontent.com",
+  ".twitter.com",
+  ".w3schools.com",
+  ".dropbox.com",
+  ".poloniex.com"
 ];
 
 var RULES_SG = [
-      ".google.com",
-      ".google.com.hk",
-      ".google.com.sg"
+  ".google.com",
+  ".google.com.hk",
+  ".google.com.sg"
 ];
 /**
  * @return {string}
  */
 function FindProxyForURL(url, host) {
 
-    function isDomain(domain) {
-        var host_length, domain_length;
-        return ((domain[0] === '.') ? (host === domain.slice(1) || ((host_length = host.length) >= (domain_length = domain.length) && host.slice(host_length - domain_length) === domain)) : (host === domain));
-    }
+  function isDomain(domain) {
+    var host_length, domain_length;
+    return ((domain[0] === '.') ? (host === domain.slice(1) || ((host_length = host.length) >= (domain_length = domain.length) && host.slice(host_length - domain_length) === domain)) : (host === domain));
+  }
 
-    function rule_filter(callback, r) {
-        // IMPORTANT: Respect the order of RULES.
-        for (var j = 0; j < r.length; j++) {
-            var rule = r[j];
-              if (callback(rule) === true) {
-                  return true;
-              }
-        }
-        return false;
+  function rule_filter(callback, r) {
+    // IMPORTANT: Respect the order of RULES.
+    for (var j = 0; j < r.length; j++) {
+      var rule = r[j];
+      if (callback(rule) === true) {
+        return true;
+      }
     }
-    alert("find proxy "+ url + " " + host);
-    if (rule_filter(isDomain, RULES) === true){
-        return PROXY_METHOD;
-    } else if (rule_filter(isDomain, RULES_SG) === true){
-        return PROXY_DROPLET_SG;
-    } else {
-        return "DIRECT";
-    }
+    return false;
+  }
+
+  alert("find proxy " + url + " " + host);
+  if (rule_filter(isDomain, RULES) === true) {
+    return PROXY_METHOD;
+  } else if (rule_filter(isDomain, RULES_SG) === true) {
+    return PROXY_DROPLET_SG;
+  } else {
+    return "DIRECT";
+  }
 
 }
