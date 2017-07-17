476592e8ad66556b7cb58646343bef37f8152066
Youze Tang <tangyouze@gmail.com>
2017-07-17 11:00:39 +0800

    bug fix

--- new_aliyun/run.sh
+++ new_aliyun/run.sh
@@ -7,6 +7,6 @@ sudo apt update
 sudo apt upgrade
 sudo mkdir -p /opt/bin
 sudo wget https://raw.githubusercontent.com/qbtrade/dotfiles/master/new_aliyun/new_user.py -O /opt/bin/new_user.py
-sudo wget https://raw.githubusercontent.com/qbtrade/dotfiles/master/new_aliyun/zshrc.sh.j2 -O /opt/bin/new_user.py
+sudo wget https://raw.githubusercontent.com/qbtrade/dotfiles/master/new_aliyun/zshrc.sh.j2 -O /home/zshrc
 
 
