1f42ea69f77b94a82b415b24f219959b12313a85
Youze Tang <tangyouze@gmail.com>
2017-08-05 14:00:37 +0800

    update

---
+++ jupyter/outputarea.js
@@ -0,0 +1,11 @@
+
+
+
+    define([
+        'notebook/js/outputarea'
+    ], function(outputarea) {
+      console.log('outputarea', outputarea)
+    });
+
+// seems can be modify here
+// http://jupyter-notebook.readthedocs.io/en/latest/extending/frontend_extensions.html
