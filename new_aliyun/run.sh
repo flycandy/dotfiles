fd05dfffd00fedaa17d0dd5353c0100a9e56fbf5
Youze Tang <tangyouze@gmail.com>
2017-09-21 20:00:37 +0800

    Update run.sh

--- new_aliyun/run.sh
+++ new_aliyun/run.sh
@@ -6,7 +6,8 @@
 # install docker
 sudo apt update
 sudo apt upgrade -y
-sudo apt install wget curl ifstat htop python python3 python-pip python3-pip -y
+sudo apt install wget curl ifstat htop python python3 python-pip python3-pip -y 
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev
 sudo mkdir -p /opt/bin
 sudo pip install docopt
 sudo pip3 install docopt
